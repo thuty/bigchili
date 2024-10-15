cc63fe4675c49b7996850effbaa98b2043a2409e
Danilo <141171771+thuty@users.noreply.github.com>
2024-10-15 11:23:14 -0300

    aaaaa

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ function criacartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML= `
-    
+    <div class="conteudo-cartao">
+    <h3>categoria</h3>
+    <div class="pergunta-cartao">
+        <p>pergunta</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>resposta</p>
+    </div>
+</div>
     `
+    container.appendChild(cartao);
 }
